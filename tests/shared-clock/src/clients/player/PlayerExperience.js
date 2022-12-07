f1f15630b0bc3663ed67cdcd91534f44efdce0da
b-ma <benjamin.matuszewski@gmail.com>
2022-12-07 15:08:08 +0100

    chore: fix shared-clock test

---
+++ tests/shared-clock/src/clients/player/PlayerExperience.js
@@ -0,0 +1,206 @@
+import { AbstractExperience } from '@soundworks/core/client';
+import { render, html, nothing } from 'lit-html';
+import renderInitializationScreens from '@soundworks/template-helpers/client/render-initialization-screens.js';
+
+import { Scheduler } from 'waves-masters';
+import Transport from '../../../../../src/Transport.js';
+
+import '@ircam/simple-components/sc-text.js';
+import '@ircam/simple-components/sc-button.js';
+import '@ircam/simple-components/sc-number.js';
+import '@ircam/simple-components/sc-toggle.js';
+import '@ircam/simple-components/sc-bang.js';
+import '../lib/sc-clock.js';
+
+class PlayerExperience extends AbstractExperience {
+  constructor(client, config, $container) {
+    super(client);
+
+    this.config = config;
+    this.$container = $container;
+    this.rafId = null;
+
+    this.hasControls = window.location.hash === '#controller';
+
+    this.sync = this.require('sync');
+
+    renderInitializationScreens(client, config, $container);
+  }
+
+  async start() {
+    super.start();
+
+    this.transport = await this.client.stateManager.attach('transport');
+
+    this.scheduler = new Scheduler(() => this.sync.getSyncTime());
+
+    this.clock = new Transport(this.scheduler);
+    this.clock.setState(this.transport.get('transportState'));
+
+    this.preRoll = new Transport(this.scheduler);
+
+    this.transport.subscribe(updates => {
+      this.render();
+      this.updateEngine(updates);
+    }, true);
+
+    window.addEventListener('resize', () => this.render());
+    this.render();
+  }
+
+  updateEngine(updates) {
+    for (let [key, value] of Object.entries(updates)) {
+      switch (key) {
+        case 'clockEvents': {
+          const clockEvents = value;
+
+          if (clockEvents === null) {
+            break;
+          }
+
+          clockEvents.map(event => this.clock.addEvent(event));
+
+          if (updates.preRollEvents) {
+            const preRollEvents = updates.preRollEvents;
+            preRollEvents.map(event => this.preRoll.addEvent(event));
+          }
+          break;
+        }
+        case 'loopStart': {
+          this.clock.loopStart = value;
+          break;
+        }
+        case 'loopEnd': {
+          this.clock.loopEnd = value;
+          break;
+        }
+        // case 'loop': {
+        //   this.clock.loop = value;
+        //   break;
+        // }
+      }
+    }
+  }
+
+  render() {
+    const width = Math.min(window.innerWidth - 40, 614); // padding
+
+    render(html`
+      <div style="padding: 20px">
+        <div>
+          <sc-clock
+            style="margin: 4px auto; display: block; width: ${width}px; /* this is needed for centering */"
+            .getTimeFunction="${() => {
+              const now = this.sync.getSyncTime();
+              return this.preRoll.getPositionAtTime(now);
+            }}"
+            font-size="20"
+            twinkle="[0, 0.5]"
+            width="${width}"
+          ></sc-clock>
+          <sc-clock
+            style="margin: 4px auto; display: block; width: ${width}px; /* this is needed for centering */"
+            .getTimeFunction="${() => {
+              const now = this.sync.getSyncTime();
+              return this.clock.getPositionAtTime(now);
+            }}"
+            font-size="20"
+            twinkle="[0.5, 1]"
+            width="${width}"
+          ></sc-clock>
+        </div>
+
+        ${this.hasControls ?
+          html`
+            <div style="width: ${width}px; margin: 50px auto 0;">
+              <div style="padding: 4px 0;">
+                <sc-button
+                  style="margin-bottom: 4px"
+                  value="start"
+                  @input="${e => this.transport.set({ command: 'start' })}"
+                ></sc-button>
+                <sc-button
+                  style="margin-bottom: 4px"
+                  value="pause"
+                  @input="${e => this.transport.set({ command: 'pause' })}"
+                ></sc-button>
+                <sc-button
+                  style="margin-bottom: 4px"
+                  value="stop"
+                  @input="${e => this.transport.set({ command: 'stop' })}"
+                ></sc-button>
+              </div>
+              <!-- <hr /> -->
+              <div style="padding: 4px 0;">
+                <sc-text
+                  readonly
+                  value="seek"
+                ></sc-text>
+                <sc-number
+                  value="${this.transport.get('seekPosition')}"
+                  @change="${e => this.transport.set({
+                    command: 'seek',
+                    seekPosition: e.detail.value,
+                  })}"
+                ></sc-number>
+                <sc-bang
+                  @input="${e => this.transport.set({ command: 'seek' })}"
+                ></sc-bang>
+              </div>
+              <!-- <hr /> -->
+              <div style="padding: 4px 0;">
+                <sc-text
+                  readonly
+                  value="pre-roll"
+                ></sc-text>
+                <sc-toggle
+                  .value="${this.transport.get('enablePreRoll')}"
+                  @change="${e => this.transport.set({ enablePreRoll: e.detail.value })}"
+                ></sc-toggle>
+                <sc-number
+                  value="${this.transport.get('preRollDuration')}"
+                  @change="${e => this.transport.set({ preRollDuration: e.detail.value })}"
+                ></sc-number>
+              </div>
+              <div style="padding: 4px 0;">
+                <sc-text
+                  readonly
+                  value="loop start"
+                ></sc-text>
+                <sc-number
+                  value="${this.transport.get('loopStart')}"
+                  @change="${e => this.transport.set({ loopStart: e.detail.value })}"
+                ></sc-number>
+              </div>
+              <div style="padding: 4px 0;">
+                <sc-text
+                  readonly
+                  value="loop end"
+                ></sc-text>
+                <sc-number
+                  value="${this.transport.get('loopEnd')}"
+                  @change="${e => this.transport.set({ loopEnd: e.detail.value })}"
+                ></sc-number>
+              </div>
+              <div style="padding: 4px 0;">
+                <sc-text
+                  readonly
+                  value="loop"
+                ></sc-text>
+                <sc-toggle
+                  ?value="${this.transport.get('loop')}"
+                  @change="${e => this.transport.set({
+                    command: 'loop',
+                    loop: e.detail.value
+                  })}"
+                ></sc-toggle>
+              </div>
+            </div>
+          ` : nothing
+        }
+      </div>
+    `, this.$container);
+  }
+}
+
+export default PlayerExperience;
---
+++ tests/shared-clock/src/server/index.js
@@ -0,0 +1,206 @@
+import 'source-map-support/register';
+import { Server } from '@soundworks/core/server';
+import path from 'path';
+import serveStatic from 'serve-static';
+import compile from 'template-literal';
+
+import pluginSyncFactory from '@soundworks/plugin-sync/server';
+
+import PlayerExperience from './PlayerExperience.js';
+import transportSchema from './schemas/transport.js';
+
+import { Scheduler } from 'waves-masters';
+// import { getTime } from '@ircam/sc-gettime';
+
+import getConfig from '../utils/getConfig.js';
+const ENV = process.env.ENV || 'default';
+const config = getConfig(ENV);
+const server = new Server();
+
+// html template and static files (in most case, this should not be modified)
+server.templateEngine = { compile };
+server.templateDirectory = path.join('.build', 'server', 'tmpl');
+server.router.use(serveStatic('public'));
+server.router.use('build', serveStatic(path.join('.build', 'public')));
+server.router.use('vendors', serveStatic(path.join('.vendors', 'public')));
+
+console.log(`
+--------------------------------------------------------
+- launching "${config.app.name}" in "${ENV}" environment
+- [pid: ${process.pid}]
+--------------------------------------------------------
+`);
+
+(async function launch() {
+  let mod = await import('../../../../src/Transport.js');
+  const Transport = mod.default;
+
+  mod = await import('@ircam/sc-gettime');
+  const { getTime } = mod;
+
+  // -------------------------------------------------------------------
+  // register plugins
+  // -------------------------------------------------------------------
+  server.pluginManager.register('sync', pluginSyncFactory, {
+    getTimeFunction: getTime,
+  }, []);
+
+  // -------------------------------------------------------------------
+  // register schemas
+  // -------------------------------------------------------------------
+  server.stateManager.registerSchema('transport', transportSchema);
+
+  try {
+    await server.init(config, (clientType, config, httpRequest) => {
+      return {
+        clientType: clientType,
+        app: {
+          name: config.app.name,
+          author: config.app.author,
+        },
+        env: {
+          type: config.env.type,
+          websockets: config.env.websockets,
+          subpath: config.env.subpath,
+        }
+      };
+    });
+
+    const sync = server.pluginManager.get('sync');
+    const scheduler = new Scheduler(() => sync.getSyncTime());
+    const clock = new Transport(scheduler);
+
+    const transport = await server.stateManager.create('transport', {
+      transportState: clock.getState(),
+    });
+
+    server.stateManager.registerUpdateHook('transport', (updates, currentValues) => {
+      if (updates.command) {
+        const { command } = updates;
+        const { enablePreRoll, preRollDuration } = currentValues;
+        const applyAt = sync.getSyncTime() + 0.1;
+
+        const clockEvents = [
+          {
+            type: 'cancel',
+            time: applyAt,
+          },
+        ];
+
+        switch (command) {
+          case 'start':
+            clockEvents.push({
+              type: 'play',
+              time: enablePreRoll ? applyAt + preRollDuration : applyAt,
+            });
+            break;
+          case 'stop':
+            clockEvents.push({
+              type: 'pause',
+              time: applyAt,
+            });
+            clockEvents.push({
+              type: 'seek',
+              time: applyAt,
+              position: 0,
+            });
+            break;
+          case 'pause':
+            clockEvents.push({
+              type: 'pause',
+              time: applyAt,
+            });
+            break;
+          case 'seek':
+            clockEvents.push({
+              type: 'seek',
+              time: applyAt,
+              position: updates.seekPosition || currentValues.seekPosition,
+            });
+            break;
+          case 'loop':
+            clockEvents.push({
+              type: 'loop',
+              time: applyAt,
+              loop: updates.loop,
+            });
+            break;
+        }
+
+        const preRollEvents = [{
+          type: 'cancel',
+          time: applyAt,
+        }];
+
+        if (enablePreRoll && clockEvents.length > 0) {
+          if (command === 'start') {
+            preRollEvents.push({
+              type: 'seek',
+              time: applyAt,
+              position: -1 * (preRollDuration + 1),
+            })
+            preRollEvents.push({
+              type: 'play',
+              time: applyAt,
+            });
+            preRollEvents.push({
+              type: 'pause',
+              time: applyAt + preRollDuration,
+            });
+            preRollEvents.push({
+              type: 'seek',
+              time: applyAt + preRollDuration,
+              position: 0,
+            });
+          } else {
+            // for seek pause and stop, we want to stop the playroll now
+            preRollEvents.push({
+              type: 'pause',
+              time: applyAt,
+            });
+            preRollEvents.push({
+              type: 'seek',
+              time: applyAt,
+              position: 0,
+            });
+          }
+        }
+
+        return {
+          ...updates,
+          clockEvents,
+          preRollEvents,
+        };
+      }
+    });
+
+
+    const playerExperience = new PlayerExperience(server, 'player');
+    // start all the things
+    await server.start();
+    playerExperience.start();
+
+    transport.subscribe(updates => {
+      if (updates.clockEvents) {
+        updates.clockEvents.map(event => clock.addEvent(event));
+      }
+    });
+
+    const engine = {
+      onScheduledEvent(event, position) {
+        const transportState = clock.getState();
+        transport.set({ transportState });
+      }
+    };
+
+    clock.add(engine);
+
+  } catch (err) {
+    console.error(err.stack);
+  }
+})();
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('> Unhandled Promise Rejection');
+  console.log(reason);
+});
