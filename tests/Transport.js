ebdf0e088eddf3ddb8e478fb11870096e7192cb5
b-ma <benjamin.matuszewski@gmail.com>
2022-12-07 14:41:12 +0100

    test(transport): fixed

--- tests/Transport.js
+++ tests/Transport.js
@@ -3,30 +3,55 @@ import { assert } from 'chai';
 import { Scheduler } from 'waves-masters';
 import Transport from '../src/Transport.js';
 
+describe(`Transport`, () => {
+  it(`should work`, function() {
+    this.timeout(6 * 1000);
+    return new Promise(resolve => {
 
-const startTime = Date.now() / 1000;
-const scheduler = new Scheduler(() => (Date.now() / 1000) - startTime);
+      const startTime = Date.now() / 1000;
+      const scheduler = new Scheduler(() => (Date.now() / 1000) - startTime);
 
-const transport = new Transport(scheduler);
+      const transport = new Transport(scheduler);
 
-const player = {
-  onScheduledEvent(event, position, audioTime, dt) {
-    console.log(event.type, position, audioTime, dt);
-  },
-};
+      const expectedEvents = [
+        { type: 'play', position: 0, time: 1 },
+        { type: 'seek', position: 23.5, time: 2 },
+        { type: 'pause', position: 24.5, time: 3 },
+        { type: 'seek', position: 0, time: 3 },
+        { type: 'play', position: 0, time: 5 }, // this one wont be precise
+      ];
 
-transport.add(player);
+      let index = 0;
 
-transport.play(1);
-transport.seek(2, 23.5); // seek at position 23.5
-// stop and seek to zero
-transport.pause(3);
-transport.seek(3, 0);
+      const player = {
+        onScheduledEvent(event, position, time, dt) {
+          const expected = expectedEvents[index];
+          assert.equal(event.type, expected.type);
+          assert.equal(event.position, expected.position);
+          assert.isBelow(Math.abs(event.time - expected.time), 0.01);
+
+          console.log(`type: ${event.type} - position: ${position} - time: ${time}`);
+
+          index += 1;
 
 
-setTimeout(() => {
-  console.log('restart');
-  transport.play(Date.now() / 1000 - startTime + 1);
-  // scheduler.remove(transport);
-  // // scheduler.remove(player);
-}, 4 * 1000);
+          if (index === expectedEvents.length) {
+            resolve();
+          }
+        },
+      };
+
+      transport.add(player);
+
+      transport.play(1);
+      transport.seek(2, 23.5); // seek at position 23.5
+      // stop and seek to zero
+      transport.pause(3);
+      transport.seek(3, 0);
+
+      setTimeout(() => {
+        transport.play(Date.now() / 1000 - startTime + 1);
+      }, 4 * 1000);
+    });
+  });
+});
